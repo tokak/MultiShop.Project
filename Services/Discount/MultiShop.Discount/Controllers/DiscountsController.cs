8cb76988b1f48b608e12d180af9429e2752b1a60
Murat Tokak <tokakmurat01@gmail.com>
2025-10-03 15:47:09 +0300

    Orm aracı dapper ile api işlemleri

---
+++ Services/Discount/MultiShop.Discount/Controllers/DiscountsController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Discount.Dtos;
+using MultiShop.Discount.Services;
+
+namespace MultiShop.Discount.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DiscountsController : ControllerBase
+    {
+        private readonly IDiscountService _discountService;
+
+        public DiscountsController(IDiscountService discountService)
+        {
+            _discountService = discountService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> DiscountCouponList()
+        {
+            var values = await _discountService.GetAllCouponAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("id")]
+        public async Task<IActionResult> GetDiscountCouponById(int id)
+        {
+            var values = await _discountService.GetByIdCouponAsync(id);
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateDiscountCoupon(CreateCouponDto  createCouponDto)
+        {
+            await _discountService.CreateCouponAsync(createCouponDto);
+            return Ok("Başarıyla eklendi.");
+        }
+        [HttpDelete]
+        public async Task<IActionResult> DeleteDiscountCoupon(int id)
+        {
+            await _discountService.DeleteCouponAsync(id);
+            return Ok("Başarıyla silindi");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateDiscountCoupon(UpdateCouponDto updateCouponDto)
+        {
+            await _discountService.UpdateCouponAsync(updateCouponDto);
+            return Ok("Başarıyla güncellendi");
+        }
+
+    }
+
+}
---
+++ Services/Discount/MultiShop.Discount/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.Data.SqlClient;
+using MultiShop.Discount.Context;
+using MultiShop.Discount.Services;
+using System.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddTransient<DapperContext>();
+
+builder.Services.AddScoped<IDiscountService, DiscountService>();
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
